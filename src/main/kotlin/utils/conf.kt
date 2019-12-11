707061fbcdc961d11add5a4570f7ff2fb6ad142e
Riwal <riwal.lefaou7@gmail.com>
2019-12-11 08:23:03 +0000

    Delete conf.kt

--- src/main/kotlin/utils/conf.kt
+++
@@ -1,7 +0,0 @@
-package utils
-
-const val USERNAME = "admin"
-const val PASSWORD = "Mifb}KEO6H5I4>`1Jy(Z"
-const val DATABASE_HOST = "51.159.27.223"
-const val DATABASE_PORT = "5156"
-const val DATABASE_NAME = "user"
